158f3d8601a728cc40db6c96d1ac99a0fe56daef
osyrisrblx <osyrisrblx@gmail.com>
2020-01-23 21:16:24 -0800

    Move default properties out of function

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { Workspace } from "@rbxts/services";
 
+const DEFAULT_PROPERTIES: Partial<BasePart> = {
+	Anchored: true,
+	Position: new Vector3(0, -0.5, 0),
+	Size: new Vector3(512, 1, 512),
+	BackSurface: Enum.SurfaceType.Universal,
+	BottomSurface: Enum.SurfaceType.Universal,
+	FrontSurface: Enum.SurfaceType.Universal,
+	LeftSurface: Enum.SurfaceType.Universal,
+	RightSurface: Enum.SurfaceType.Universal,
+	TopSurface: Enum.SurfaceType.Universal,
+	Parent: Workspace,
+};
+
 export function createBaseplate(settings?: Partial<Part>) {
-	const baseplate = new Instance("Part");
-	baseplate.Anchored = true;
-	baseplate.Position = new Vector3(0, 0, 0);
-	baseplate.Size = new Vector3(500, 1, 500);
-	baseplate.BackSurface = Enum.SurfaceType.Universal;
-	baseplate.BottomSurface = Enum.SurfaceType.Universal;
-	baseplate.FrontSurface = Enum.SurfaceType.Universal;
-	baseplate.LeftSurface = Enum.SurfaceType.Universal;
-	baseplate.RightSurface = Enum.SurfaceType.Universal;
-	baseplate.TopSurface = Enum.SurfaceType.Universal;
-	baseplate.Parent = Workspace
-	Object.assign(baseplate, settings);
-	return baseplate;
+	return Object.assign(new Instance("Part"), DEFAULT_PROPERTIES, settings);
 }
